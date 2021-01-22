58678cc2e2a3df581e0e03e22466239e4342498c
Patrick <psoumani@gmail.com>
2021-01-22 00:51:47 -0500

    Just Run tested

--- TestGreatestNumber.java
+++ TestGreatestNumber.java
@@ -7,7 +7,7 @@ public class TestGreatestNumber {
     @Test
     public void testGreatestNumber() {
         assertEquals(25, GreatestNumber.greatest(12, 25, 23));
-        assertEquals(5, GreatestNumber.greatest(3, 5, 5));
+        assertEquals(5, GreatestNumber.greatest(3, 7, 5));
     }
 
 }
